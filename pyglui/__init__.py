d90dac3ae760a6ac20d3aab80641416e0097e610
Pablo Prietz <pablo@prietz.org>
2018-06-13 18:49:01 +0200

    Add ETRA Graph class

--- pyglui/__init__.py
+++ pyglui/__init__.py
@@ -1,1 +1,2 @@
 __version__ = '1.22'
+__etra__ = 'ETRA'
--- pyglui/graph.pyx
+++ pyglui/graph.pyx
@@ -297,7 +297,168 @@ cdef class Line_Graph:
         gl.glMatrixMode(gl.GL_MODELVIEW)
 
 
+cdef class ETRA_Graph:
+    cdef fs.Context glfont
+    cdef readonly double[::1] data
+    cdef public float avg, bar_width, min_val, max_val, legend_margin
+    cdef float scale
+    cdef int idx, d_len
+    cdef int x, y, win_height, win_width
+    cdef basestring label
+    cdef int s_idx,s_size
+    cdef object data_source
+    cdef public RGBA color
+    cdef public int legend_idx
 
+
+    def __cinit__(self,int data_points = 50,float min_val = 0, float max_val = 100):
+        self.data = view.array(shape=(data_points,), itemsize=sizeof(double), format="d")
+        self.d_len = data_points
+        self.label = 'Title %0.2f units'
+        self.bar_width = 2
+        self.min_val = min_val
+        self.max_val = max_val
+        self.scale = 1.
+        self.legend_idx = 0
+        self.legend_margin = 50.
+
+        self.glfont = fs.Context()
+        self.glfont.add_font('opensans',get_opensans_font_path())
+        self.glfont.set_size(int(18 * self.scale))
+        self.glfont.set_align(fs.FONS_ALIGN_TOP | fs.FONS_ALIGN_RIGHT)
+        self.color = RGBA(0.5, 0.8, 0.75,.5)
+
+    def __init__(self,int data_points = 25,float min_val = 0, float max_val = 100):
+        cdef int x
+        for x in range(data_points):
+            self.data[x] = 0
+        self.avg = 0
+        self.data_source = lambda: 0
+
+    def adjust_window_size(self, w, h):
+        self.win_width = w
+        self.win_height = h
+
+    property scale:
+        def __get__(self):
+            return self.scale
+
+        def __set__(self,val):
+            self.scale = val
+            self.glfont.set_size(int(18*self.scale))
+
+    property label:
+        def __get__(self):
+            return self.label
+        def __set__(self,new_label):
+            self.label = new_label
+
+    property pos:
+        def __get__(self):
+            return self.x,self.y
+
+        def __set__(self,val):
+            self.x,self.y = val
+
+    property update_rate:
+        def __set__(self,r):
+            self.s_size = r
+            self.s_idx = 0
+
+        def __get__(self):
+            return self.s_size
+
+    property update_fn:
+        def __set__(self,fn):
+            self.data_source = fn
+
+
+    def add(self,double val):
+        self.s_idx = (self.s_idx +1) %self.s_size
+        if self.s_idx == 0:
+            self.data[self.idx] = val
+            self.idx = (self.idx +1) %self.d_len
+            #very rough avg estimation
+            self.avg += (val-self.avg)*(5./self.d_len)
+
+    def update(self):
+        cdef float val
+        self.s_idx = (self.s_idx +1) %self.s_size
+        if self.s_idx == 0:
+            val = self.data_source()
+            self.data[self.idx] = val
+            self.idx = (self.idx +1) %self.d_len
+            #very rough avg estimation
+            self.avg += (val-self.avg)*(2./self.d_len)
+
+
+    def draw(self):
+        cdef int i
+
+        cdef int x, y, width, height
+        x = int(self.x * self.scale)
+        y = int(self.y * self.scale)
+        width = int(self.win_width - 2 * x - self.legend_margin * self.scale)
+        height = int(self.win_height - 2 * y)
+
+        gl.glPushAttrib(gl.GL_VIEWPORT_BIT)
+        gl.glViewport(x, y, width, height)
+
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glPushMatrix()  # projection matrix
+
+        gl.glLoadIdentity()  # graph coodinates:
+        gl.glOrtho(0, self.d_len - 1,  # x dim
+                   self.min_val, self.max_val,  # y dim
+                   -1, 1)
+
+        # --- graph start
+
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+        gl.glPushMatrix()  # model matrix
+        gl.glLoadIdentity()
+
+        gl.glLineWidth(self.bar_width*self.scale)
+        gl.glColor4f(self.color.r,self.color.g,self.color.b,self.color.a)
+
+        gl.glBegin(gl.GL_LINE_STRIP)
+        for i in range(self.idx,self.d_len):
+            gl.glVertex3f(i - self.idx, self.data[i], 0)
+
+        for i in range(self.idx):
+            gl.glVertex3f(i + (self.d_len - self.idx), self.data[i], 0)
+        gl.glEnd()
+        gl.glPopMatrix()  # model matrix
+
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glPopMatrix()  # model matrix
+        gl.glPopAttrib()
+
+        # --- graph end / legend start
+
+        x = width
+        width = int(self.legend_margin * self.scale)
+
+        gl.glPushAttrib(gl.GL_VIEWPORT_BIT)
+        gl.glViewport(x, y, width, height)
+
+        gl.glPushMatrix()  # projection matrix
+        gl.glLoadIdentity()  # margin coordinates
+        gl.glOrtho(0, width, height, 0, -1, 1)
+
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+        gl.glPushMatrix()  # model matrix
+        gl.glLoadIdentity()
+
+        self.glfont.set_color_float(self.color[:])
+        self.glfont.draw_text(width, self.legend_idx * 18 * self.scale, self.label)
+
+        gl.glPopMatrix()  # model matrix
+
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glPopMatrix()  # projection matrix
+        gl.glPopAttrib()
+        gl.glMatrixMode(gl.GL_MODELVIEW)
 
 cdef class Averaged_Value:
     cdef fs.Context glfont
