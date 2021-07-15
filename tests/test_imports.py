85b0c0eb2036b0039a73b61ee4b3913466f38169
Pablo Prietz <pablo@prietz.org>
2021-07-15 12:01:51 +0200

    Add more test cases

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,7 +1,23 @@
+def test_import_numpy():
+    import numpy
+
+
 def test_import_pyglui():
     import pyglui
 
     assert pyglui
+
+
+def test_import_cygl():
+    import pyglui.cygl
+
+
+def test_import_cygl_shader():
+    from pyglui.cygl import shader
+
+
+def test_import_cygl_utils():
+    from pyglui.cygl import utils
 
 
 def test_import_ui():
